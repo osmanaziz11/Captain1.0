0aaed89d6bdcb07bf3bb5d6ac3f140ad45d96241
Osman <osmanaziz012@gmail.com>
2023-07-04 06:44:42 +0500

    Login Layout fixed

--- renderer/components/Login/LoginForm.js
+++ renderer/components/Login/LoginForm.js
@@ -6,27 +6,24 @@ function LoginForm({ handler }) {
       <div className="flex justify-center items-center flex-col   pb-10 transition  cursor-pointer">
         <div className="w-[370px] p-10 flex flex-col justify-center items-center  rounded h-[370px] bg-[#1b1b1b]  transition cursor-pointer">
           <h1 className="text-gray-300 font-medium">
-            Please provide credentials
+            PlEASE PROVIDE CREDENTIALS
           </h1>
-          <div className="relative w-full  pe-2  ">
+          <div className="relative w-full  pe-2  mt-10">
             <input
               type="text"
-              className={`outline-none text-center bg-[#1b1b1b] text-gray-400 text-sm rounded block w-full pl-10 p-2.5 transition   placeholder-gray-400 `}
+              className={`outline-none text-center bg-[#1b1b1b] border border-zinc-950 text-gray-400 text-sm rounded block w-full pl-10 p-2.5 transition   placeholder-gray-400 `}
               placeholder="Enter username"
             />
           </div>
-          <p className=" font-normal text-red-500 text-xs  h-[20px]  ">
-            {/* {true && true.message} */}
-          </p>
           <div className="relative w-full  pe-2  ">
             <input
               type="password"
-              className={`outline-none text-center bg-[#1b1b1b] text-gray-300 text-sm rounded block w-full pl-10 p-2.5 transition   placeholder-gray-400 `}
+              className={`outline-none text-center bg-[#1b1b1b] border border-zinc-950 mt-2 text-gray-300 text-sm rounded block w-full pl-10 p-2.5 transition   placeholder-gray-400 `}
               placeholder="Enter password"
             />
           </div>
-          <p className=" font-normal text-red-500 text-xs  h-[20px]  ">
-            {/* {true && true.message} */}
+          <p className=" font-normal text-red-500 text-xs  h-[30px] my-2 opacity-0 ">
+            Lorem ipsum dolor sit amet consectetur et?
           </p>
           <button
             type="submit"
